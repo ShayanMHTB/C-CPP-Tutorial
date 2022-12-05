c0d9795954df0e889a379965617dcb6ff752009c
ShayanMHTB <shayan.mhtb@gmail.com>
2022-12-05 01:08:37 +0100

    HelloWorld Application in C/C++

---
+++ HelloWorld/hello-world.c
@@ -0,0 +1,9 @@
+// HelloWorld/hello-world.c
+
+#include <stdio.h>
+
+int main() {
+   printf("Hello, World!\n");
+   printf("C Programming Language is awesome!\n");
+   return 0;
+}
---
+++ HelloWorld/hello-world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n";
+  cout << "C++ Programming Language is awesome!\n";
+}
